9de81041b74dd33077548616ec75ca3a4a340a7c
HAVVING <sparkle5406@gmail.com>
2021-09-26 23:53:00 +0900

    "/"경로에서 문자열을 출력하는 웹 Application

--- src/go.tutorial/hello.go
+++ src/go.tutorial/hello.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// "/" 경로로 접속했을 때 처리할 핸들러 함수 지정
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// "Hello, World!" 문자열을 화면에 출력
+		fmt.Println(w, "Hello, World!")
+	})
+
+	// 8000 포트로 웹 서버 구동
+	http.ListenAndServe(":8000", nil)
 }
