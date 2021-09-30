8ee276a3c1d7cb6eed1109a2d420cc23dc13d8ab
HAVVING <sparkle5406@gmail.com>
2021-09-30 22:01:46 +0900

    미들웨어(Middleware)

--- src/main/main.go
+++ src/main/main.go
@@ -2,34 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	r := &router{make(map[string]map[string]HandlerFunc)}
 
 	r.HandleFunc("GET", "/", func(c *Context) {
+		t := time.Now()
 		fmt.Fprintln(c.ResponseWriter, "Hello, World!")
+		log.Printf("[%s] %q %v\n", c.Request.Method, c.Request.URL.String(), time.Now().Sub(t))
 	})
 
 	r.HandleFunc("GET", "/about", func(c *Context) {
+		t := time.Now()
 		fmt.Fprintln(c.ResponseWriter, "About")
+		log.Printf("[%s] %q %v\n", c.Request.Method, c.Request.URL.String(), time.Now().Sub(t))
 	})
 
-	r.HandleFunc("GET", "/user/:id", func(c *Context) {
-		fmt.Fprintf(c.ResponseWriter, "Get User %v\n", c.Params["id"])
-	})
+	r.HandleFunc("GET", "/user/:id", logHandler(recoverHandler(func(c *Context) {
+		if c.Params["id"] == "0" {
+			fmt.Fprintf(c.ResponseWriter, "Get User %v\n", c.Params["id"])
+		}
+	})))
 
 	r.HandleFunc("GET", "/user/:user_id/address/:address_id", func(c *Context) {
 		fmt.Fprintf(c.ResponseWriter, "Get User %v's Address %v\n", c.Params["user_id"], c.Params["address_id"])
 	})
 
-	r.HandleFunc("POST", "/users", func(c *Context) {
-		fmt.Fprintf(c.ResponseWriter, "Create User\n")
-	})
+	r.HandleFunc("POST", "/users", logHandler(recoverHandler(parseFormHandler(parseJsonBodyHandler(func(c *Context) {
+		fmt.Fprintln(c.ResponseWriter, c.Params)
+	})))))
 
 	r.HandleFunc("POST", "/users/:user_id/addresses", func(c *Context) {
-		fmt.Fprintf(c.ResponseWriter, "Create User %v's Address\n", c.Params["user_id"])
+		t := time.Now()
+		fmt.Fprintf(c.ResponseWriter, "Create User %v's Address %v\n", c.Params["user_id"], c.Params["address_id"])
+		log.Printf("[%s] %q %v\n", c.Request.Method, c.Request.URL.String(), time.Now().Sub(t))
 	})
 
 	// 8080 포트로 웹 서버 구동
