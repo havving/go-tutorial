32919e050e39952a93d74214069385c127c7e58e
HAVVING <sparkle5406@gmail.com>
2021-10-02 22:32:23 +0900

    Renderer

--- src/main/context.go
+++ src/main/context.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
 
 /** 컨텍스트 타입 정의 **/
 type Context struct {
@@ -12,3 +17,66 @@ type Context struct {
 
 /** 핸들러 타입 정의 **/
 type HandlerFunc func(*Context)
+
+/** JSON 포맷으로 데이터 렌더링 **/
+func (c *Context) RenderJson(v interface{}) {
+	// HTTP Status를 StatusOK로 지정
+	c.ResponseWriter.WriteHeader(http.StatusOK)
+	// Content-Type을 application/json으로 지정
+	c.ResponseWriter.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	// v 값을 json으로 출력
+	if err := json.NewEncoder(c.ResponseWriter).Encode(v); err != nil {
+		// 에러 발생 시 RenderErr 메서드 호출
+		c.RenderErr(http.StatusInternalServerError, err)
+	}
+}
+
+/** XML 포맷으로 데이터 렌더링 **/
+func (c *Context) RenderXml(v interface{}) {
+	// HTTP Status를 StatusOK로 지정
+	c.ResponseWriter.WriteHeader(http.StatusOK)
+	// Content-Type을 application/xml으로 지정
+	c.ResponseWriter.Header().Set("Content-Type", "application/xml; charset=utf-8")
+
+	// v 값을 json으로 출력
+	if err := json.NewEncoder(c.ResponseWriter).Encode(v); err != nil {
+		// 에러 발생 시 RenderErr 메서드 호출
+		c.RenderErr(http.StatusInternalServerError, err)
+	}
+}
+
+/** 에러 상태를 HTTP Status로 렌더링 **/
+func (c *Context) RenderErr(code int, err error) {
+	if err != nil {
+		if code > 0 {
+			// 정상적인 code를 전달하면 HTTP Status를 해당 code로 지정
+			http.Error(c.ResponseWriter, http.StatusText(code), code)
+		} else {
+			// 정상적인 code가 아니면 HTTP Status를 StatusInternalServerError로 지정
+			defaultErr := http.StatusInternalServerError
+			http.Error(c.ResponseWriter, http.StatusText(defaultErr), defaultErr)
+		}
+	}
+}
+
+// templates: 템플릿 객체를 보관하기 위한 map
+var templates = map[string]*template.Template{}
+
+/** 템플릿 렌더링 **/
+func (c *Context) RenderTemplate(path string, v interface{}) {
+	// path에 해당하는 템플릿이 있는지 확인
+	t, ok := templates[path]
+	if !ok {
+		// path에 해당하는 템플릿이 없으면 템플릿 객체 생성
+		t = template.Must(template.ParseFiles(filepath.Join(".", path)))
+		templates[path] = t
+	}
+	// v 값을 템플릿 내부로 전달하여 만들어진 최종 결과를 c.ResponseWriter에 출력
+	t.Execute(c.ResponseWriter, v)
+}
+
+/** Redirect 기능 **/
+func (c *Context) Redirect(url string) {
+	http.Redirect(c.ResponseWriter, c.Request, url, http.StatusMovedPermanently)
+}
--- src/main/main.go
+++ src/main/main.go
@@ -6,6 +6,11 @@ import (
 	"time"
 )
 
+type User struct {
+	Id        string
+	AddressId string
+}
+
 func main() {
 	// 서버 생성
 	s := NewServer()
@@ -14,6 +19,7 @@ func main() {
 		t := time.Now()
 		fmt.Fprintln(c.ResponseWriter, "Hello, World!")
 		log.Printf("[%s] %q %v\n", c.Request.Method, c.Request.URL.String(), time.Now().Sub(t))
+		c.RenderTemplate("/public/index.html", map[string]interface{}{"time": time.Now()})
 	})
 
 	s.HandleFunc("GET", "/about", func(c *Context) {
@@ -23,12 +29,14 @@ func main() {
 	})
 
 	s.HandleFunc("GET", "/user/:id", logHandler(recoverHandler(func(c *Context) {
-		if c.Params["id"] == "0" {
-			fmt.Fprintf(c.ResponseWriter, "Get User %v\n", c.Params["id"])
-		}
+		u := User{Id: c.Params["id"].(string)}
+		c.RenderXml(u)
+		fmt.Fprintf(c.ResponseWriter, "Get User %v\n", c.Params["id"])
 	})))
 
 	s.HandleFunc("GET", "/user/:user_id/address/:address_id", func(c *Context) {
+		u := User{c.Params["user_id"].(string), c.Params["address_id"].(string)}
+		c.RenderJson(u)
 		fmt.Fprintf(c.ResponseWriter, "Get User %v's Address %v\n", c.Params["user_id"], c.Params["address_id"])
 	})
 
